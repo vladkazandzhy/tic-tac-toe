81e84a45e6822010c9ef13efd5bfccea5a1d5505
Vladyslav Victorovich Kazandzhi <kazandzhy@churchofjesuschrist.org>
2020-02-24 11:14:34 -0700

    load code

---
+++ src/Game.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./Game.css";
+
+import Board from "./components/Board";
+
+export default class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
